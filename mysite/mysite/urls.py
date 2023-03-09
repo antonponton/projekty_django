c73aa701a619e6247161a9b2ff284c549171437c
Antoni Godlejewski <antoni.dzionk@interia.pl>
2023-03-09 17:21:47 +0100

    fixed settings and urls in mysite

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,12 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
+]
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+urlpatterns += [
+    re_path(r'^site/(?P<path>.*)$', serve,
+        {'document_root': os.path.join(BASE_DIR, 'site'),
+         'show_indexes': True},
+        name='site_path'
+        ),
 ]
 
 urlpatterns += [
