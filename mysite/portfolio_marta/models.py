ec2b29e5029d127f7d33a03589b57bd829b5ad16
Antoni Godlejewski <antoni.dzionk@interia.pl>
2023-03-09 13:53:51 +0100

    add Art class in models.py

--- mysite/portfolio_marta/models.py
+++ mysite/portfolio_marta/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+class Art(models.Model) :
+    title = models.Charfield(max_length=200, validators = [MinLengthValidator(2,"Title must be greater than 2 characters")])
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    picture = models.BinaryField(null=True, editable=True)
+    content_type = models.CharField(max_length=256, null=True, help_text='The MIMEType of the file')
+
+    def __str__(self):
+        return self.title
