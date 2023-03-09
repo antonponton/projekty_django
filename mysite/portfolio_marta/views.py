949373d5cd8c7a10ab92d0efd9ce20365944e270
Antoni Godlejewski <antoni.dzionk@interia.pl>
2023-03-09 17:34:52 +0100

    edit views and change url to portfolio

--- mysite/portfolio_marta/views.py
+++ mysite/portfolio_marta/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from portfolio_marta.models import Art
+from django.views.generic import ListView
 
-from django.views import View
-
-class ArtListView(View):
+class ArtListView(ListView):
     model = Art
     template_name = "portfolio_marta/art_list.html"
