5982ab436e45a82207016f6e0d483bfb5f79de9c
Antoni Godlejewski <antoni.dzionk@interia.pl>
2023-03-09 16:58:29 +0100

    edit views.py in portfolio_marta

--- mysite/portfolio_marta/views.py
+++ mysite/portfolio_marta/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from portfolio_marta.models import Art
 
-# Create your views here.
+from django.views import View
+
+class ArtListView(View):
+    model = Art
+    template_name = "portfolio_marta/art_list.html"
