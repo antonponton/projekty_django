f0e142af999cad3cca19147a4cf80d1b07d219a8
Antoni Godlejewski <antoni.dzionk@interia.pl>
2023-03-10 14:02:00 +0100

    add forms.py

---
+++ mysite/portfolio_marta/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from portfolio_marta.models import Art
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+class CreateForm(forms.ModelForm):
+    max_upload_limit = 2 * 1024 * 1024
+    max_upload_limit_text = max_upload_limit
+
+    picture = forms.FileField(required=False, label='File to Upload <= '+max_upload_limit_text)
+    upload_field_name = 'picture'
+
+    class Meta:
+        model = Art
+        fields = ['title', 'text', 'picture',] 
+
+    # Validate the size of the picture
+    def clean(self):
+        cleaned_data = super().clean()
+        pic = cleaned_data.get('picture')
+        if pic is None:
+            return
+        if len(pic) > self.max_upload_limit:
+            self.add_error('picture', "File must be < "+self.max_upload_limit_text+" bytes")
+
+    # Convert uploaded File object to a picture
+    def save(self, commit=True):
+        instance = super(CreateForm, self).save(commit=False)
+
+        # We only need to adjust picture if it is a freshly uploaded file
+        f = instance.picture   # Make a copy
+        if isinstance(f, InMemoryUploadedFile):  # Extract data from the form to the model
+            bytearr = f.read()
+            instance.content_type = f.content_type
+            instance.picture = bytearr  # Overwrite with the actual image data
+
+        if commit:
+            instance.save()
+            self.save_m2m()
+
+        return instance
