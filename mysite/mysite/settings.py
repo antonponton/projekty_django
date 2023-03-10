f1e5744acdd67ff584c10ac414edd3e3abfd3aa8
Antoni Godlejewski <antoni.dzionk@interia.pl>
2023-03-10 15:57:47 +0100

    changed settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,9 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 APP_NAME = 'Anton Site' 
 
 # Quick-start development settings - unsuitable for production
