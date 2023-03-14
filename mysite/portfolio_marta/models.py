51596b45687205444b8f5eb3fdd940384096de3a
Antoni Godlejewski <antoni.dzionk@interia.pl>
2023-03-14 16:30:21 +0100

    add owner in model art

--- mysite/portfolio_marta/models.py
+++ mysite/portfolio_marta/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
+from django.conf import settings
 
 class Art(models.Model) :
     title = models.CharField(max_length=200, validators = [MinLengthValidator(2,"Title must be greater than 2 characters")])
@@ -8,6 +9,7 @@ class Art(models.Model) :
     updated_at = models.DateTimeField(auto_now=True)
     picture = models.BinaryField(null=True, editable=True)
     content_type = models.CharField(max_length=256, null=True, help_text='The MIMEType of the file')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='art_owned')
 
     def __str__(self):
         return self.title
