67a0a0d497da1bd4b574e2f90f85974ee08e310b
Antoni Godlejewski <antoni.dzionk@interia.pl>
2023-03-15 15:11:51 +0100

    change list view and font

--- mysite/portfolio_marta/views.py
+++ mysite/portfolio_marta/views.py
@@ -12,6 +12,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class ArtListView(OwnerListView):
     model = Art
     template_name = "portfolio_marta/art_list.html"
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['type_list'] = Type.objects.all()
+        return context
 
 class ArtDetailView(OwnerDetailView):
     model = Art
