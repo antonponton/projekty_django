d9fca880b091f93611bb1019572baca9de25e8d6
Antoni Godlejewski <antoni.dzionk@interia.pl>
2023-06-23 10:34:37 +0200

    add tests for genre, language and book models

--- locallibrary/catalog/tests/test_models.py
+++ locallibrary/catalog/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from catalog.models import Author
+from catalog.models import Author, Genre, Language, Book
 
 class AuthorModelTest(TestCase):
     @classmethod
@@ -45,3 +45,148 @@ class AuthorModelTest(TestCase):
     def test_get_absolute_url(self):
         author = Author.objects.get(id=1)
         self.assertEqual(author.get_absolute_url(), '/catalog/author/1')
+
+class GenreModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Genre.objects.create(name='Fantasy')
+    
+    def test_name_label(self):
+        genre = Genre.objects.get(id=1)
+        field_label = genre._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'name')
+    
+    def test_name_max_length(self):
+        genre = Genre.objects.get(id=1)
+        max_length = genre._meta.get_field('name').max_length
+        self.assertEqual(max_length, 200)
+        
+    def test_help_text(self):
+        genre = Genre.objects.get(id=1)
+        help_text = genre._meta.get_field('name').help_text
+        self.assertEqual(help_text, 'Enter a book genre (e.g. Science Fiction)')
+
+    def test_object_name_is_name(self):
+        genre = Genre.objects.get(id=1)
+        self.assertEqual(str(genre), genre.name)
+
+class LanguageModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Language.objects.create(name='English')
+    
+    def test_name_label(self):
+        language = Language.objects.get(id=1)
+        field_label = language._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'name')
+    
+    def test_name_max_length(self):
+        language = Language.objects.get(id=1)
+        max_length = language._meta.get_field('name').max_length
+        self.assertEqual(max_length, 200)
+        
+    def test_help_text(self):
+        language = Language.objects.get(id=1)
+        help_text = language._meta.get_field('name').help_text
+        self.assertEqual(help_text, 'Enter a book language (e.g. English)')
+
+    def test_object_name_is_name(self):
+        language = Language.objects.get(id=1)
+        self.assertEqual(str(language), language.name)
+
+class BookModelTest(TestCase):
+    def setUp(self):
+        test_author = Author.objects.create(first_name='John', last_name='Smith')
+        test_genre1 = Genre.objects.create(name='Fantasy')
+        test_genre2 = Genre.objects.create(name='Sci-Fi')
+        test_genre3 = Genre.objects.create(name='Romance')
+        test_genre4 = Genre.objects.create(name='Children')
+        test_language = Language.objects.create(name='English')
+        test_book = Book.objects.create(
+            title='Book Title',
+            summary='My book summary',
+            isbn='ABCDEFG',
+            author=test_author,
+            language=test_language,
+        )
+        genre_objects_for_book = Genre.objects.all()
+        test_book.genre.set(genre_objects_for_book)
+        test_book.save()
+    
+    def test_title_label(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field('title').verbose_name
+        self.assertEqual(field_label, 'title')
+
+    def test_summary_label(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field('summary').verbose_name
+        self.assertEqual(field_label, 'summary')
+
+    def test_isbn_label(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field('isbn').verbose_name
+        self.assertEqual(field_label, 'ISBN')
+
+    def test_author_label(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field('author').verbose_name
+        self.assertEqual(field_label, 'author')
+
+    def test_language_label(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field('language').verbose_name
+        self.assertEqual(field_label, 'language')
+
+    def test_genre_label(self):
+        book = Book.objects.get(id=1)
+        field_label = book._meta.get_field('genre').verbose_name
+        self.assertEqual(field_label, 'genre')
+
+    def test_title_max_length(self):
+        book = Book.objects.get(id=1)
+        max_length = book._meta.get_field('title').max_length
+        self.assertEqual(max_length, 200)
+
+    def test_summary_max_length(self):
+        book = Book.objects.get(id=1)
+        max_length = book._meta.get_field('summary').max_length
+        self.assertEqual(max_length, 1000)
+
+    def test_isbn_max_length(self):
+        book = Book.objects.get(id=1)
+        max_length = book._meta.get_field('isbn').max_length
+        self.assertEqual(max_length, 13)
+
+    def test_summary_help_text(self):
+        book = Book.objects.get(id=1)
+        help_text = book._meta.get_field('summary').help_text
+        self.assertEqual(help_text, 'Enter a brief description of the book')
+
+    def test_isbn_help_text(self):
+        book = Book.objects.get(id=1)
+        help_text = book._meta.get_field('isbn').help_text
+        self.assertEqual(help_text, '13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+
+    def test_genre_help_text(self):
+        book = Book.objects.get(id=1)
+        help_text = book._meta.get_field('genre').help_text
+        self.assertEqual(help_text, 'Select a genre for this book')
+
+    def test_language_help_text(self):
+        book = Book.objects.get(id=1)
+        help_text = book._meta.get_field('language').help_text
+        self.assertEqual(help_text, 'Select a language for this book')
+
+    def test_object_name(self):
+        book = Book.objects.get(id=1)
+        self.assertEqual(str(book), book.title)
+
+    def test_get_absolute_url(self):
+        book = Book.objects.get(id=1)
+        self.assertEqual(book.get_absolute_url(), '/catalog/book/1')
+
+    def test_display_genre(self):
+        book = Book.objects.get(id=1)
+        self.assertEqual(book.display_genre(), 'Fantasy, Sci-Fi, Romance')
+
